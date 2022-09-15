8c60f4b046867fa8d40fdebd2c107936894b056c
Queniex <queniesalbiyah@gmail.com>
2022-09-15 19:13:11 +0700

    Learning about filestream

---
+++ app.js
@@ -0,0 +1,14 @@
+// Core Module
+// File System
+
+const fs = require('fs')
+
+// menuliskan string ke file (synchronous)
+try {
+    fs.writeFileSync('test.txt', 'Hello World secara synchronous!')
+} catch (e) {
+    console.log(e)
+}
+
+// menuliskan string ke file (Asynchronous)
+fs.writeFile('')
